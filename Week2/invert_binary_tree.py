df7337685d1afca496aca198a6fdedbfd8546975
punxund <punxund@naver.com>
2022-11-17 17:53:51 +0100

    third one

--- Week2/invert_binary_tree.py
+++ Week2/invert_binary_tree.py
@@ -10,13 +10,14 @@ class TreeNode:
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         #if root is empty, return None
-        if root is None:
+        if root is None: 
             return None
 
         #change left side and right side with recursive function
-        x = self.invertTree(root.left)
-        y = self.invertTree(root.right)
+        x = self.invertTree(root.left) #O(n)?
+        y = self.invertTree(root.right) #O(n)?
         root.right = x
         root.left = y
         
+        #O(n)
         return root
---
+++ Week2/range_sum_of_bst.py
@@ -0,0 +1,32 @@
+#https://leetcode.com/problems/range-sum-of-bst/description/
+#2022-11-17 Hongsik Kim
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int: 
+        #make a node list of the range between low and higt
+        list_dfs = []
+        #search all nodes and append on the list if the node in the range
+        def find_node(root) :
+            if root is None:
+                return None
+            find_node(root.left) #O(n)
+            find_node(root.right) #O(n)
+            if root.val >= low and root.val <= high:
+                list_dfs.append(root.val)
+        #function start
+        find_node(root)
+        
+        return sum(list_dfs) #O(n)
+
+
+
+        
+        
+        
+        
