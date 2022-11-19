ef75ee14ae3ed9dc5c61a5b9bea9d8908b177e7b
punxund <punxund@naver.com>
2022-11-19 02:14:00 +0100

    sixth one

---
+++ Week2/time_needed_to_inform_all_employees.py
@@ -0,0 +1,21 @@
+#https://leetcode.com/problems/time-needed-to-inform-all-employees/
+#2022-11-18 Hongsik Kim
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        time_list = [0]*n
+        time_list[headID] = 0        
+        
+        def time_update(n, manager, headID, informTime, time_list):
+            for i in range(0, n): #O(n)
+                if time_list[i] == 0 and i != headID:
+                    if manager[i] == headID:
+                        time_list[i] = time_list[headID] + informTime[headID]
+                        time_update(n, manager, i, informTime, time_list) #O(n)
+
+        time_update(n, manager, headID, informTime, time_list)
+
+        return max(time_list)
+
+        
+
